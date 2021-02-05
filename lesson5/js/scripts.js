4256474d822e707a6743135be53a52ac3e980afd
AlexxCourter <alex.courter@gmail.com>
2021-02-05 13:41:11 -0500

    Finished updating version 1 of Preston page

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,3 +1,4 @@
+//menu button toggle to open nav menu
 function toggleMenu() {
     document.getElementById("headNav").classList.toggle("hide");
 }
@@ -8,3 +9,13 @@ const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var today = days[save_date.getDay()] + ", " + save_date.getDate() + " " + months[save_date.getMonth()] + " " + save_date.getFullYear()
 document.getElementById("today-date").textContent = today;
+
+//display message for Saturday pancakes ONLY if it is friday
+const dayNumber = save_date.getDay(); //use Date() object above
+
+const element = document.getElementById("message");
+if (dayNumber == 5) {
+    element.classList.add("showme");
+} else {
+    element.classList.add("hideme");
+}
