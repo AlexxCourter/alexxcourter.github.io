fba2f5bc17eab94d1a4efcce3c3d84ba83eef398
AlexxCourter <alex.courter@gmail.com>
2021-02-13 16:12:29 -0500

    fixed wind chill to display proper math

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -5,7 +5,7 @@ const tempNumber = parseFloat(document.getElementById("temp").textContent);
 
 const speedNumber = parseFloat(document.getElementById("speed").textContent);
 
-let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, 0.16) + (0.4275 * tempNumber) * (Math.pow(speedNumber, 0.16)));
+let windchill = 35.74 + (0.6215 * tempNumber) - 35.75 * Math.pow(speedNumber, 0.16) + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
 windchill = Math.round(windchill)
 console.log(windchill);
 
